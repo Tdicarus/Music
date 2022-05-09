81e0d4a4d7b7615028e1245bab295d585e899a6b
Tdicarus <ngt679@gmail.com>
2022-05-09 20:38:55 +0700

    a

---
+++ app/src/main/java/cntt61/sv61133478/mylyrics/MainActivity.java
@@ -0,0 +1,13 @@
+package cntt61.sv61133478.mylyrics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/cntt61/sv61133478/mylyrics/ShowLyricsActivity.java
@@ -0,0 +1,8 @@
+package cntt61.sv61133478.mylyrics;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ShowLyricsActivity extends AppCompatActivity
+{
+    
+}
